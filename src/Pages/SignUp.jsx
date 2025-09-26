eda3b63c60ff655a4fea9b9804412575dd868206
sahincoder <sahincoderbd@gmail.com>
2025-09-26 08:12:39 +0600

    user created and send data to the database successfully

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -6,16 +6,42 @@ const SignUp = () => {
 
     const handleSignUp = (e) => {
         e.preventDefault();
-        console.log(e);
         const form = e.target;
-        console.log(form);
         const email = form.email.value;
+        const name = form.name.value;
         const password = form.password.value;
-        console.log( email, password, 'user wants to sign up');
         createUser(email, password)
             .then((result) => {
-                // Signed up 
-                console.log(result);
+
+               const userCreationTime=result?.user?.metadata?.creationTime;
+               const userLastSignIn=result?.user?.metadata?.lastSignInTime;
+
+               const user={
+                name,
+                password,
+                email,
+                userCreationTime,
+                userLastSignIn
+               }
+
+               if(user){
+                fetch('http://localhost:5000/newUser',{
+                method:"POST",
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(user),
+               }
+               )
+               .then(res=>res.json())
+               .then(data=>{
+                console.log(data);
+                if(data.insertedId){
+                    console.log('user successfully added to the database');
+                }
+               })
+               }
+            
                 // ...
             })
             .catch((error) => {
@@ -39,11 +65,14 @@ const SignUp = () => {
                     <div className="card-body">
                         <form onSubmit={handleSignUp} className="fieldset">
                     
+                            <label className="label text-white text-lg">Name</label>
+                            <input type="text" name='name' className="input placeholder:text-gray-500 text-black bg-white w-full" placeholder="Your Name" />
+
                             <label className="label text-white text-lg">Email</label>
-                            <input type="email" name='email' className="input placeholder:text-gray-800 text-black bg-white w-full" placeholder="Email" />
+                            <input type="email" name='email' className="input placeholder:text-gray-500 text-black bg-white w-full" placeholder="Email" />
 
                             <label className="label text-white text-lg">Password</label>
-                            <input type="password" name='password' className="input bg-white/50 text-black w-full placeholder:text-gray-800" placeholder="Password" />
+                            <input type="password" name='password' className="input bg-white/50 text-black w-full placeholder:text-gray-500" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">SignUp</button>
                         </form>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDTELQHPAeb9y_cozpV74Li9BrKk2nqOKA",
-  authDomain: "coffee-shophub.firebaseapp.com",
-  projectId: "coffee-shophub",
-  storageBucket: "coffee-shophub.firebasestorage.app",
-  messagingSenderId: "1045930732848",
-  appId: "1:1045930732848:web:58b889c03ca0f628052cf5",
-  measurementId: "G-4TLD63QF15"
-};
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
+}  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
