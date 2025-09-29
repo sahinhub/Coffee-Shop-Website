c4d2722043b455f0d9915f7ae3261a4fb57511c6
sahincoder <sahincoderbd@gmail.com>
2025-09-29 06:44:39 +0600

    an error fixed

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+    build: {
+    chunkSizeWarningLimit: 1000, // increase limit from 500 KB to 1000 KB
+  },
+
 })
